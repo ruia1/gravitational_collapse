8445432b79474e12aaf599002ba26293b8bd8ecd
ruia1 <past1215@gmail.com>
2024-01-07 04:15:49 +0900

    add visualization process of gravitation_collapse1

--- gravitational_collapse1.py
+++ gravitational_collapse1.py
@@ -1,6 +1,6 @@
 '''
 ver : Python 3.7.3 64-bit
-last change : 24/1/4 5:30 AM
+last change : 24/1/7 3:35 AM
 '''
 
 import numpy as np
@@ -11,23 +11,45 @@ a = 1.0
 
 m = 1.0
 p = 0.0
-n = 3.0 * m / 4 / a^3
+n = 3.0 * m / 4 / a ** 3
 rho = n
 U = 0
 r = a
-t_stop = 100.0
+t_stop = 1.14
 
-def osModel(t,r,rho,n,U):
-    dr,drho,dn,dU = np.zeros_like([r,rho,n,U])
-    S = n * r^2
+def osModel(t,state):
+    r,rho,n,U = state
+    dstate = np.zeros_like(state)
+    dr,drho,dn,dU = dstate
+    S = n * r ** 2
 
     dr = U
     dS = -U / r * S
-    dn = dS / r^2 -2 * n * dr / r
+    dn = dS / (r ** 2) -2 * n * dr / r
     drho = rho * dn / n
-    dU = -m/r^2
+    dU = -m/(r ** 2)
 
-    return [dr,drho,dn,dU]
+    dstate = np.array([dr,drho,dn,dU])
+    return dstate
 
 dt = 0.01
 t = np.arange(0, t_stop, dt)
+
+state = np.array([r,rho,n,U])
+
+y = np.empty((len(t), 4))
+y[0] = np.array([r,rho,n,U])
+flg = True
+for i in range(1, len(t)):
+    y[i] = y[i-1] + osModel(t[i-1],y[i-1]) * dt
+    if flg and y[i,0]<=0:
+        print(t[i],y[0])
+
+fig = plt.figure(figsize=(5,4))
+
+ax = fig.add_subplot(111)
+ax.plot(t,y[:,0],label="r")
+
+plt.legend()
+
+plt.show()
