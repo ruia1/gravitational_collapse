7d7724c02d2c26a9d8ca4cc424bf5146891c86b0
ruia1 <past1215@gmail.com>
2024-02-09 02:21:17 +0900

    不正な値の回避

--- gravitational_collapse1.py
+++ gravitational_collapse1.py
@@ -1,22 +1,22 @@
 '''
 ver : Python 3.7.3 64-bit
-last change : 24/2/5
+last change : 24/2/8
 '''
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.integrate import solve_ivp
+#from scipy.integrate import solve_ivp
 
 a = 1.0
 da = 0.01
-len_a = int((a+da)/da)
+len_a = int(a/da-1)
 
-m = np.arange(0.0,a+da,da)
+m = np.arange(da,a,da)
 p = 0.0
-n = np.array([3.0 * m[i] / 4 / a ** 3 for i in range(len_a)])
+n = np.array([4.0  / 3 * np.pi / len_a for i in range(len_a)])
 rho = np.copy(n)
 U = np.array([0.0 for i in range(len_a)])
-r = np.array([i * da for i in range(len_a)])
+r = np.array([(i + 1) * da for i in range(len_a)])
 
 t_stop = 10.0
 
@@ -42,17 +42,18 @@ def osModel(t,state,m,dash_r,dash_U):
     dstate = np.zeros_like(state)
     dr,drho,dn,dU = dstate
     S = n * r ** 2
+    remove_nan = lambda x:np.where(np.isnan(x)|np.isinf(x),0,x)
 
-    dr = U
-    dS = -dash_U / dash_r * S
-    dn = dS / (r ** 2) -2 * n * dr / r
-    drho = rho * dn / n
-    dU = -m/(r ** 2)
+    dr = remove_nan(U)
+    dS = remove_nan(-dash_U / dash_r * S)
+    dn = remove_nan(dS / (r ** 2) -2 * n * dr / r)
+    drho = remove_nan(rho * dn / n)
+    dU = remove_nan(-m/(r ** 2))
 
     dstate = np.array([dr,drho,dn,dU])
     return dstate
 
-dt = 0.01
+dt = 0.0001
 t = np.arange(0, t_stop, dt)
 
 state = np.array([r,rho,n,U])
@@ -65,14 +66,14 @@ for i in range(1, len(t)):
     dash_U = dash(y[i-1,3],da)
     for j in range(len_a):
         y[i,:,j] = y[i-1,:,j] + osModel(t[i-1],y[i-1,:,j],m[j],dash_r[j],dash_U[j]) * dt
-    if y[i,0,-1]<=0:
+    if y[i,0,-1]<=0.001:
         print(t[i],y[i:0])
-        print(y[:,0,0])
-        print(y[:,0,len_a//2])
+        #print(y[:,0,-1])
+        #print(y[:,0,len_a//2])
         stop_t = i
         t_stop = t[i]
         break
-
+y[:,0] = np.where(y[:,0]<0, None, y[:,0])
 fig = plt.figure(figsize=(5,4))
 
 ax = fig.add_subplot(111)
